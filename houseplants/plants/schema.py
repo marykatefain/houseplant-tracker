fa0d34e1c4be84a1adcc88803e5f60a6ca2c7a39
Mary Kate Fain <marykate@promptworks.com>
2019-01-17 17:38:11 -0500

    add speciesType to graphql

--- houseplants/plants/schema.py
+++ houseplants/plants/schema.py
@@ -1,6 +1,10 @@
-from houseplants.plants.models import Plant
+from houseplants.plants.models import Plant, Species
 from graphene_django import DjangoObjectType
 import graphene
+
+class SpeciesType(DjangoObjectType):
+    class Meta:
+        model = Species
 
 class PlantType(DjangoObjectType):
     class Meta:
@@ -8,8 +12,12 @@ class PlantType(DjangoObjectType):
 
 class Query(graphene.ObjectType):
     plants = graphene.List(PlantType)
+    species = graphene.List(SpeciesType)
 
     def resolve_plants(self, info):
         return Plant.objects.all()
+
+    def resolve_species(self, info):
+        return Species.objects.all()
 
 schema = graphene.Schema(query=Query)
