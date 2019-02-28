6a7837560614e66a4b57e8a8a6e28072655aba57
Mary Kate Fain <marykate@promptworks.com>
2019-02-28 16:02:53 -0500

    load static files

--- houseplants/plants/views.py
+++ houseplants/plants/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
+from django.template.response import TemplateResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the plants index.")
+    return TemplateResponse(request, "index.html", {})
