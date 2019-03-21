eaa50c074fb882a5bfd9dc93011510ded76ea1a0
Mary Kate Fain <marykate@promptworks.com>
2019-03-21 17:00:51 -0400

    starting template of plant list

--- houseplants/plants/js/components/Plants.vue
+++ houseplants/plants/js/components/Plants.vue
@@ -2,7 +2,20 @@
   <div class="hello">
     <h1>PLANTS VUE WOO</h1>
     <a @click="fetchPlants">Get the plants</a>
-    {{ plants }}
+    <div v-for="plant in plants">
+      <div>
+        {{ plant.nickName }}
+      </div>
+      <div>
+        {{ plant.species.commonName }}
+        ( {{ plant.species.scientificName}} )
+      </div>
+      <div>
+        {{ plant.location }}
+      </div>
+      <div>
+        {{ plant.species.lightRequirement }}
+      </div>
   </div>
 </template>
 
@@ -25,19 +38,23 @@ export default {
       const query = gql(`
         query{
           plants {
-            id
             nickName
+            species {
+              id
+              commonName
+              scientificName
+              lightRequirement
+            }
+            profilePic
+            location
           }
         }
       `)
 
-      client.query({ query })
+      client.query({ query, fetchPolicy: 'network-only', })
+      //TODO: change cache status on production later? This prevents cache
         .then(result => {
           this.plants = result.data.plants
-          console.log("RESULT:")
-          console.log(result.data.plants)
-          console.log("THIS DOT PLANTS:")
-          console.log(this.plants)
         })
     },
   },
